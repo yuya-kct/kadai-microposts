34003e5ce4154843842c731b561297893a80d373
yuya.tsurusaki <yuya.tsurusaki@hakuhodody-one.co.jp>
2025-06-10 22:52:49 +0900

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -15,7 +15,7 @@ class MicropostsController extends Controller
             $user = \Auth::user();
             // ユーザーの投稿の一覧を作成日時の降順で取得
             // （後のChapterで他ユーザーの投稿も取得するように変更しますが、現時点ではこのユーザーの投稿のみ取得します）
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
